4cf139ba7171d912c7c4106dbe7eff52d791a15f
bai <baiqilong@one2much.com>
2018-01-24 19:43:44 +0800

    new as

---
+++ UIDS/mainVC/modules/DetatilVC/cell/JFStarAndShareCell.swift
@@ -0,0 +1,19 @@
+//
+//  JFStarAndShareCell.swift
+//  UIDS
+//
+//  Created by bai on 16/5/26.
+//  Copyright © 2016年 bai. All rights reserved.
+//
+
+import UIKit
+
+class JFStarAndShareCell: UITableViewCell {
+
+    @IBOutlet weak var befromLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+}
